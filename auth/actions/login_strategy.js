93461a6bbcef7b059f120f27d4c0f9e808469288
Your Name <you@example.com>
2016-10-13 23:41:26 -0700

    upd

--- auth/actions/login_strategy.js
+++ auth/actions/login_strategy.js
@@ -13,37 +13,31 @@ var init = function(passport){
 
 
             var findUser = function(){
-                    User.findOne({ 'username' :  username },
-                        function(err, user) {
+                    User.findOne({ 'username' :  username })
+                    .then(function(user){
 
-                            console.log("User:" + user)
-                            console.log("Err: " + err)
-                            // In case of any error, return using the done method
-                            if (err){
-                                console.log(err)
-                                return done(err);
-                            }
-                            // Username does not exist, log the error and redirect back
-                            if (!user){
-                                console.log('User Not Found with username '+username);
-                                return done(null, false, req.flash('message', 'User Not found.'));
-                            }
-                            // User exists but wrong password, log the error
-                            if (!isValidPassword(user, password)){
-                                console.log('Invalid Password');
-                                return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
-                            }
-                            // User and password both match, return user from done method
-                            // which will be treated like success
-                            console.log("Successful Login!")
-                            return done(null, user);
+                        console.log("User:" + user)
+
+                        // Username does not exist, log the error and redirect back
+                        if (!user){
+                            console.log('User Not Found with username '+username);
+                            return done(null, false, req.flash('message', 'User Not found.'));
                         }
-                    )
+                        // User exists but wrong password, log the error
+                        if (!isValidPassword(user, password)){
+                            console.log('Invalid Password');
+                            return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                        }
+                        // User and password both match, return user from done method
+                        // which will be treated like success
+                        console.log("Successful Login!")
+                        return done(null, user);
+                    })
                     .catch(function(err){
                         console.log(err);
+                        return done(err);
                     });
             }
-
             process.nextTick(findUser);
 
         })
