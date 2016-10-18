d160a8400fd29644f136745497ebdf45c7189797
Your Name <you@example.com>
2016-10-18 13:05:36 -0700

    upd

--- pages/dashboard/recipients/update.js
+++ pages/dashboard/recipients/update.js
@@ -19,9 +19,10 @@ router.get('/:recipient_id', function(req, res){
 
 
 router.post('/', function(req, res) {
+  var phone = req.body.area_code + req.body.phone_prefix + req.body.phone_suffix
 
   var updated_user = {
-    phone: req.body.phone,
+    phone: phone,
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     interval: req.body.interval
