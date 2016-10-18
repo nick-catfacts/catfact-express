1ea2ab14c20fc0a26adc7dd279d8620083242c8a
Your Name <you@example.com>
2016-10-18 14:26:54 -0700

    upd

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,6 +4,16 @@ var lodash = require('lodash-node');
 var app_root = require('app-root-path')
 
 
+router.use( function(req, res, next){
+  res.locals.deCamelCaser = function(string){
+    var result = string.replace( /([A-Z])/g, " $1" );
+    result = result.replace('_', " ");
+    return result.charAt(0).toUpperCase() + result.slice(1);
+  }
+  next();
+});
+
+
  // Routes
 router.get('/', function(req, res) {
 
@@ -12,6 +22,7 @@ router.get('/', function(req, res) {
   var username =  res.locals.current_user.username;
   var recipients = res.locals.current_user.recipients.toObject();
   var recipient_headers = lodash.keys(recipients[0]);
+
   var messages_remaining = res.locals.current_user.account.messages_remaining;
 
   res.render(app_root + '/views/dashboard/index', {
